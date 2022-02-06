8ad1c750864a629bc673e118f3485398df2e65b7
Ratkovski <drezcry@hotmail.com>
2022-02-06 14:25:11 -0300

    codebase

---
+++ src/test/java/com/example/kafka/producer/demo/kafka/producer/DemoKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.producer.demo.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
